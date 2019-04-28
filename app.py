eadaf7afc18315ca566d7fba683924ad790051c4
karolisjan <karolis.jankauskas@gmail.com>
2019-04-28 23:10:06 +0100

    Add basic figures

--- app.py
+++ app.py
@@ -13,23 +13,32 @@ import layout
 dash_app = Dash(__name__)
 dash_app.server.name = 'IndPenSim'
 dash_app.config['supress_callback_exceptions'] = True
-# dash_app.index_string = '''
-#     <!DOCTYPE html>
-#     <html>
-#         <head>
-#             {%meta%}
-#             <link rel="shortcut icon" type="image/png" href"assets/icon.png"/>
-#             {%css%}
-#         </head>
-#         <body>
-#             {%app_entry%}
-#             <footer>
-#                 {%config%}
-#                 {%scripts%}
-#             </footer>
-#         </body
-#     </html>
-# '''
+dash_app.index_string = '''
+    <!DOCTYPE html>
+    <html>
+        <head>
+            {%meta%}
+            <title>IndPenSim Batch Data</title>
+            <!-- link rel="shortcut icon" type="image/png" href"assets/icon.png"/ -->
+            {%css%}
+        </head>
+        <body>
+            {%app_entry%}
+            <footer>
+                {%config%}
+                {%scripts%}
+            </footer>
+        </body
+    </html>
+'''
+
+for css in [
+    'https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css',
+]:
+    dash_app.css.append_css({'external_url': css})
+    
+
 dash_app.layout = html.Div([
     dcc.Location(
         id='url',
@@ -44,6 +53,11 @@ dash_app.layout = html.Div([
     ]
 )
 def pass_batch_id_to_button(batch_id):
+    '''
+        Handles dropdown value selection events, 
+        i.e. when a value is selected from the 
+        list, it is passed on to a button.
+    '''
     return batch_id
 
 
@@ -55,10 +69,21 @@ def pass_batch_id_to_button(batch_id):
     ]
 )
 def refresh_page(n_clicks, batch_id):
+    '''
+        Handles button click events, i.e.
+        when a button is clicked (n_clicks
+        changes) button's state (or value)
+        is passed to the page's location.
+    '''
     return '/' + batch_id
 
 
 def display_page(url, cache):
+    '''
+        Updates the page with content 
+        from the cache retrieved using
+        the url.
+    '''
     page = cache.get(url)
 
     if page is None:
@@ -70,8 +95,6 @@ def display_page(url, cache):
 
 
 if __name__ == '__main__':
-    data = pd.read_csv('data/sample.csv')
-
     cache = Cache(
         dash_app.server,
         config=dict(
@@ -82,6 +105,8 @@ if __name__ == '__main__':
     )
 
     cache.clear()
+    data = pd.read_csv('data/sample.csv')
+    cache.set('data', data, timeout=0)
     batch_ids = set(['batch_{}'.format(batch_id) for batch_id in data['Batch ID']])
     cache.set('batch_ids', batch_ids, timeout=0)
 
--- layout.py
+++ layout.py
@@ -4,26 +4,32 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 
+def __make_header(batch_id, batch_ids):
+    return html.Div([
+        dcc.Dropdown(
+            id='dropdown',
+            options=[
+                { 'label': value.replace('_', ' ').title(), 'value': value } 
+                for value in sorted(list(batch_ids))
+            ],
+            value=batch_id
+        ),
+
+        html.Br([]),
+
+        html.Button('Refresh', id='button', value=batch_id)
+    ], className='row')
+
+
 def decorate(make):
     def wrapper(cache, batch_id):
+        batch_ids = cache.get('batch_ids')
+        page = make(cache, batch_id)
+
         return html.Div([
-            dcc.Dropdown(
-                id='dropdown',
-                options=[
-                    { 'label': value, 'value': value } 
-                    for value in sorted(list(cache.get('batch_ids')))
-                ],
-                value=batch_id
-            ),
-
+            __make_header(batch_id, batch_ids),
             html.Br([]),
-
-            html.Button('Refresh', id='button', value=batch_id),
-
-            html.Br([]),
-
-            # Actual page for a batch
-            make(cache, batch_id)
+            page
         ]) 
 
     return wrapper
@@ -31,14 +37,103 @@ def decorate(make):
 
 @decorate
 def make(cache, batch_id):
-    batch_cache = cache.get(batch_id)
+    data = cache.get('data')
     
-    if batch_cache is None:
+    if data is None:
         return html.Div([])
+
+    batch_data = data[data['Batch ID'] == int(batch_id.split('_')[-1])].sort_values('Time', ascending=False)
+    variables = batch_data.columns[1:31]
+    raman_spectra = batch_data.columns[34:]
 
     return html.Div([
         # Plot for variables
-
+        html.Div([
+            dcc.Graph(
+                id='variable_plot',
+                figure={
+                    'data': [
+                        go.Scatter(
+                            x=batch_data['Time'],
+                            y=batch_data[variable],
+                            line={
+                                'width': 1
+                            },
+                            mode='lines',
+                            name=variable,
+                            visible='legendonly'
+                        ) for variable in variables
+                    ],
+                    'layout': go.Layout(
+                        font={
+                            'family': 'Arial',
+                            'size': 9
+                        },
+                        showlegend=True,
+                        titlefont={
+                            'family': 'Arial',
+                            'size': 9,
+                        },
+                        xaxis={
+                            'title': 'Time',
+                            'showline': True,
+                            'zeroline': False
+                        },
+                        yaxis={
+                            'title': 'Value',
+                            'showline': True,
+                            'zeroline': False
+                        }
+                    )
+                },
+                config={
+                    'displayModeBar': False
+                }
+            )
+        ], className='six columns'),
 
         # Plot for Raman spectra
-    ])
+        html.Div([
+            dcc.Graph(
+                id='raman_plot',
+                figure={
+                    'data': [
+                        go.Scatter(
+                            x=batch_data['Time'],
+                            y=batch_data[x],
+                            line={
+                                'width': 1
+                            },
+                            mode='lines',
+                            hoverinfo=None,
+                            name=x,
+                        ) for x in raman_spectra
+                    ],
+                    'layout': go.Layout(
+                        font={
+                            'family': 'Arial',
+                            'size': 9
+                        },
+                        showlegend=False,
+                        titlefont={
+                            'family': 'Arial',
+                            'size': 9,
+                        },
+                        xaxis={
+                            'title': 'Time',
+                            'showline': True,
+                            'zeroline': False
+                        },
+                        yaxis={
+                            'title': 'Value',
+                            'showline': True,
+                            'zeroline': False
+                        }
+                    )
+                },
+                config={
+                    'displayModeBar': False
+                }
+            )
+        ], className='six columns')
+    ], className='row')
