bcd43f64169d8fc0b6fdf3e63b891b8f56c01d3e
karolisjan <karolis.jankauskas@gmail.com>
2019-06-01 13:15:31 +0100

    Clean up

--- app.py
+++ app.py
@@ -122,7 +122,8 @@ if __name__ == '__main__':
 
     dash_app.run_server(
         host='0.0.0.0',
+        port=8889,
         threaded=True,
-        debug=False,
-        use_reloader=False
+        debug=True,
+        use_reloader=True
     )
